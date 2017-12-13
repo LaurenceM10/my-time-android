98ea0302a1d090abb34fc99fc00b17f89cf9f465
Nolo88 <yarin.montenegro@gmail.com>
2017-12-12 19:13:51 -0600

    Added Task and Location model

---
+++ app/src/main/java/com/example/developer/mytime/models/Location.java
@@ -0,0 +1,26 @@
+package com.example.developer.mytime.models;
+
+/**
+ * Created by developer on 12/12/2017.
+ */
+
+public class Location {
+    private Double latitude;
+    private Double longitude;
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
--- app/src/main/java/com/example/developer/mytime/models/Task.java
+++ app/src/main/java/com/example/developer/mytime/models/Task.java
@@ -1,8 +1,58 @@
 package com.example.developer.mytime.models;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.Date;
+
 /**
  * Created by developer on 7/12/2017.
  */
 
+@IgnoreExtraProperties
 public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private Location location;
+    private Date taskDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Date getTaskDate() {
+        return taskDate;
+    }
+
+    public void setTaskDate(Date taskDate) {
+        this.taskDate = taskDate;
+    }
 }
